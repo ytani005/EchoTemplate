aeae48151386aa4cc501924a5db494ce0ec357e0
谷岡祐志 <ytani@taniokayuujinoMacBook-Pro.local>
2018-12-25 01:07:56 +0900

    echo-server-sample

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+
+	"./handler"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
-	fmt.Printf("hello,world\n")
+	fmt.Printf("echo start.\n")
+	// Echoのインスタンス作る
+	e := echo.New()
+
+	// 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// ルーティング
+	e.GET("/hello", handler.MainPage())
+
+	// サーバー起動
+	e.Start(":1323") //ポート番号指定してね
 }
